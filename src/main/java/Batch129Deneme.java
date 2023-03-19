c044dd20c630aab5545e2c9f710fd2882d9c7276
esrakbc <122485941+esrakbc@users.noreply.github.com>
2023-03-20 00:12:03 +0300

    Update Batch129Deneme.java

--- src/main/java/Batch129Deneme.java
+++ src/main/java/Batch129Deneme.java
@@ -2,5 +2,6 @@ public class Batch129Deneme {
     public static void main(String[] args) {
         System.out.println("merhaba");
         System.out.println("esra branch");
+        //Github ta yazıyorum
     }
 }
